e4deccb4c56dba9eaa917162b33f77cbdb7f907b
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2014-11-25 16:52:12 -0600

    removed unnecessary files

--- python/TTbarSelectionFilter_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ttbarselectionfilter = cms.EDFilter('TTbarSelectionFilter',
-   channel     = cms.InputTag("ttbarselectionproducer"),
-   select_ee   = cms.bool(True),
-   select_mumu = cms.bool(True),
-   select_emu  = cms.bool(True),
-   Keep_all_events  = cms.bool(False)
-)
