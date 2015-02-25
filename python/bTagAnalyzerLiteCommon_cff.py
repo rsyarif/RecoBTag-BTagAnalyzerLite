d19484bcce071040dd467ab50b0a1e71abab131b
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2015-02-24 23:49:14 -0600

    reorganized configuration files

---
+++ python/bTagAnalyzerLiteCommon_cff.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+bTagAnalyzerLiteCommon = cms.PSet(
+    runSubJets               = cms.bool(False),
+    allowJetSkipping         = cms.bool(True),
+    storeEventInfo           = cms.bool(True),
+    produceJetTrackTree      = cms.bool(False), ## True if you want to keep info for tracks associated to jets
+    produceJetPFLeptonTree   = cms.bool(False), ## True if you want to keep PF lepton info
+    storeMuonInfo            = cms.bool(False), ## True if you want to keep muon info
+    storeTagVariables        = cms.bool(False), ## True if you want to keep TagInfo TaggingVariables
+    storeCSVTagVariables     = cms.bool(True),  ## True if you want to keep CSV TaggingVariables
+    MaxEta                   = cms.double(2.5),
+    MinPt                    = cms.double(20.0),
+    src                      = cms.InputTag('generator'),
+    Jets                     = cms.InputTag('ak4PFJetsCHS'),
+    FatJets                  = cms.InputTag('selectedPatJets'),
+    GroomedFatJets           = cms.InputTag('selectedPatJetsAK8PrunedPFPacked'),
+    muonCollectionName       = cms.InputTag('selectedPatMuons'),
+    triggerTable             = cms.InputTag('TriggerResults'),
+    prunedGenParticles       = cms.InputTag('prunedGenParticlesBoost'),
+    primaryVertexColl        = cms.InputTag('offlinePrimaryVertices'),
+    TriggerPathNames = cms.vstring(
+        "HLT_HT750_v*"
+    )
+)
---
+++ python/bTagAnalyzerLite_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoBTag.BTagAnalyzerLite.bTagAnalyzerLiteLegacy_cfi import *
+from RecoBTag.BTagAnalyzerLite.bTagAnalyzerLite_cfi import *
